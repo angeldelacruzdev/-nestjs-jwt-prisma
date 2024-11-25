1faf42d477f64fdb70c108bb5024c40290e593ed
Ángel De La Cruz <acruz@dubeetecnologies.onmicrosoft.com>
2024-11-25 11:30:41 -0400

    Remove cals module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { AtGuard } from './auth/guards';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 import { PermissionsModule } from './permissions/permissions.module';
-import { CaslModule } from './casls/casl.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
